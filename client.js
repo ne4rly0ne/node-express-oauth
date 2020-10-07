d4cddb41a36982c789702a54a6899b06036329a4
Andrew Popplestone <andrewpopplestone@developmentmini.home>
2020-10-07 10:34:39 +0100

    module 3

--- client.js
+++ client.js
@@ -23,9 +23,52 @@ app.use(timeout)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-/*
-Your code here
-*/
+app.get('/authorize', (req, res) => {
+	state = randomString();
+	const myURL = new URL(config.authorizationEndpoint);
+	myURL.search = new URLSearchParams({
+		response_type: "code",
+		client_id: config.clientId,
+		redirect_uri: config.redirectUri,
+		scope: 'permission:name permission:date_of_birth',
+		state
+	})
+	res.redirect(myURL.href)
+})
+
+app.get('/callback', (req, res) => {
+	const reqState  = req.query.state
+	if (state !== reqState) {
+		res.status(403).send("Error: Forbidden")
+		return
+	}
+	axios({
+		method: 'POST',
+		url: config.tokenEndpoint,
+		auth: {
+			username: config.clientId,
+			password: config.clientSecret
+		},
+		data: {
+			code: req.query.code
+		}
+	})
+	.then(response => {
+		const { access_token } = response.data
+		return axios({
+			method: 'GET',
+			url: config.userInfoEndpoint,
+			headers: {
+				authorization: `bearer ${access_token}`
+			}
+		})
+		.then(response => {
+			res.render("welcome", {
+				user: response.data
+			})
+		})
+	})
+})
 
 const server = app.listen(config.port, "localhost", function () {
 	var host = server.address().address
